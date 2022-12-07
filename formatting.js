4e32cc335800274a6fc6f861331f694b54044584
Mark Grinols <maakugrinols@gmail.com>
2022-12-07 11:35:43 -0800

    Clean up after removing sortable

--- formatting.js
+++ formatting.js
@@ -1,25 +1,32 @@
-function setText(container, txt) {
-    txt = txt.replace(',', ' ,').replace('.', ' .').replace(':', ' :').replace(';', ' ;').replace('?', ' ?1')
-    let words = txt.split(' ')
+const likelyProperNameMap = {}
 
+function setText(container, txt) {
+    txtWithSpaces = txt.replaceAll(',', ' ,').replaceAll('.', ' .').replaceAll(':', ' :').replaceAll(';', ' ;').replaceAll('?', ' ?')
+    let words = txtWithSpaces.split(' ')
+
+    // save capitalization info
     for(let i = 1; i < words.length; i++) {
         // if word is capitlized, but is not first letter or after a period, assume it's propername.
+        // of course that breaks on "God is dead."
         const word = words[i]
         likelyProperNameMap[word.toLowerCase()] = (word[0] === word[0].toUpperCase() && words[i - 1] != '.')
     }
 
-    // shuffle words, leaving punctuation in place
-    const nonPunctuationIndexes = Array.from(Array(words.length).keys()).filter(w => !isPunctuation(words[w]))
-    const shuffledIndexes = [...nonPunctuationIndexes]
-    shuffledIndexes.sort((a, b) => 0.5 - Math.random());
-    const shuffledWords = []
-    for (let i = 0; i < words.length; i++) {
-        const isPunc = !nonPunctuationIndexes.includes(i)
-        const newWord = isPunc ?  words[i] : words[shuffledIndexes.pop()]
-        shuffledWords.push( newWord )
-    }
+    shuffle = true
+    if (shuffle) {
+        // shuffle words, leaving punctuation in place
+        const nonPunctuationIndexes = Array.from(Array(words.length).keys()).filter(w => !isPunctuation(words[w]))
+        const shuffledIndexes = [...nonPunctuationIndexes]
+        shuffledIndexes.sort((a, b) => 0.5 - Math.random());
+        const shuffledWords = []
+        for (let i = 0; i < words.length; i++) {
+            const isPunc = !nonPunctuationIndexes.includes(i)
+            const newWord = isPunc ?  words[i] : words[shuffledIndexes.pop()]
+            shuffledWords.push( newWord )
+        }
 
-    words = shuffledWords
+        words = shuffledWords
+    }
 
     for (el in words) {
         txt = words[el]
@@ -44,19 +51,12 @@ function isPunctuation(str) {
 
 function setCorrectSpaces(container) {
     // add space after word unless it's next sibling is punctuation, or we're at the end of the list.
-    // note that the final '.' is outside the container atm (which prevents dropping word after the end).
-    // the goal is to have the last child not get a space added. However,
-    // sortable.js will put the 'being dragged' element into the end of the container,
-    // So if that last element is that one (identified by class name), we stop
-    // adding spaces from the 2nd to last element instead of the last element.
     const numChildren = container.childElementCount
-    const isLastElGhost = container.children[numChildren - 1].className.match("\s*" + "sortable-drag" + "\s*")
-
     for (var i = 0; i < numChildren; ++i) {
         const el = container.children[i]
         const txt = el.innerText.trim()
         el.innerText = txt
-        if (i < (numChildren - (isLastElGhost ? 2 : 1)) && !isPunctuation(container.children[i + 1].innerText)) {
+        if (i < numChildren - 1 && !isPunctuation(container.children[i + 1].innerText)) {
             el.innerText += " "
         }
     }
@@ -66,7 +66,6 @@ function restoreCapitalization(container) {
     // if word is first word or after a period, or is capitlized in likelyProperNameMap
     // make capital, else don't.
     const numChildren = container.childElementCount
-    const isLastElGhost = container.children[numChildren - 1].className.match("\s*" + "sortable-drag" + "\s*")
 
     for (var i = 0; i < numChildren; ++i) {
         const isFirst = i == 0
@@ -82,3 +81,4 @@ function restoreCapitalization(container) {
 function getChildIndex(node) {
     return Array.prototype.indexOf.call(node.parentNode.childNodes, node);
 }
+
